909f3518dbf32e87695e0edaf0a88117f82ee72e
mohmasoudialgo <mohammadmasoudialgo@gmail.com>
2025-08-03 10:31:05 +0330

    ✨fix : update regex type parameter and correct pattern for basic case in regexFaWord function

--- src/regexFaWord.js
+++ src/regexFaWord.js
@@ -2,7 +2,7 @@
  * Generate a regex pattern for matching Persian words.
  *
  * @param {Object} options
- * @param {"basic"} [options.type="basic"] - Type of regex to generate.
+ * @param {"basic"|"strict"} [options.type="basic"] - Type of regex to generate.
  * @param {number} [options.minLen=0] - Minimum allowed length.
  * @param {number} [options.maxLen=30] - Maximum allowed length.
  * @returns {RegExp}
@@ -13,7 +13,13 @@ export default function regexFaWord({ type = "basic", minLen = 2, maxLen = 30 } 
   switch (type) {
     case "basic":
       // Only Persian letters and spaces
-      pattern = `^[\\u0600-\\u06FF\\s]{${minLen},${maxLen}}$`;
+      pattern = `^[\\u0600-\\u06CC]{${minLen},${maxLen}}$`;
+      break;
+
+    case "strict":
+      // Persian letters and Persian digits, digits not allowed at beginning,
+      // No English, no special characters, no leading/trailing space
+      pattern = `^(?!\\s)(?![۰-۹])[\\u0600-\\u06FF۰-۹\\s]{${minLen},${maxLen}}(?<!\\s)$`;
       break;
 
     default:
