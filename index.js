d5d6f05f0565064748ecf6dd5cc1bac710a536b5
mohmasoudialgo <mohammadmasoudialgo@gmail.com>
2025-08-04 17:27:49 +0330

    ✨feat : export regexFaNum and regexPass from index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 export { default as regexFaWord } from "./src/regexFaWord.js";
 export { default as regexValue } from "./src/regexFaNum.js";
+export { default as regexFaNum } from "./src/regexFaNum.js";
+export { default as regexPass } from "./src/regexPassword.js";
