7125474f3653fcb1d74471ec12fa0c2260da0e34
mohmasoudialgo <mohammadmasoudialgo@gmail.com>
2025-08-05 11:22:58 +0330

    ✨feat : add support for custom regex patterns in regexFaWord function

--- src/regexFaWord.js
+++ src/regexFaWord.js
@@ -2,12 +2,13 @@
  * Generate a regex pattern for matching Persian words.
  *
  * @param {Object} options
- * @param {"basic"|"strict"|"extended"} [options.type="basic"] - Type of regex to generate.
+ * @param {"basic"|"strict"|"extended"|"custom"} [options.type="basic"] - Type of regex to generate.
  * @param {number} [options.min=0] - Minimum allowed length.
  * @param {number} [options.max=30] - Maximum allowed length.
+ * @param {string|RegExp} [options.customPattern] - Custom regex pattern (required for type: "custom").
  * @returns {RegExp}
  */
-export default function regexFaWord({ type = "basic", min = 1, max = 999999 } = {}) {
+export default function regexFaWord({ type = "basic", min = 1, max = 999999 , customPattern } = {}) {
   let pattern = "";
 
   switch (type) {
@@ -34,6 +35,15 @@ export default function regexFaWord({ type = "basic", min = 1, max = 999999 } = 
 
       pattern = `^[\\u0600-\\u06FF](?:[\\u0600-\\u06FF0-9۰-۹.,!؟\\-_:;@#$%^&*()+=\\[\\]{}"'\\/\\\\|<>~\\s]*[\\u0600-\\u06FF0-9۰-۹.,!؟\\-_:;@#$%^&*()+=\\[\\]{}"'\\/\\\\|<>~])?$`;
       break;
+
+    case "custom":
+    // استفاده از الگوی سفارشی
+    // اگر نوع "custom" انتخاب شده باشد، باید یک الگوی سفارشی ارائه شود
+
+      if (!customPattern) {
+        throw new Error(`Custom pattern must be provided for type: "${type}"`);
+      }
+      return typeof customPattern === "string" ? new RegExp(customPattern) : customPattern;
 
     default:
       throw new Error(`Invalid word regex type: ${type}`);
